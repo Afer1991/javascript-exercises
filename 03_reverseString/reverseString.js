d3d9d513ba0527833691f90cc5fc0ff1f3f06cbc
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-01-30 11:44:10 -0500

    Add solution to exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let content = "";
+    for (let i = 0; i < string.length; i++) {
+      content += string[string.length - 1 - i]
+    }
+    return content;
 };
 
 // Do not edit below this line
