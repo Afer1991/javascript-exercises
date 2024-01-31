ff9e7efcdd9ea6d10f5fa5e7816c9074d046c093
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-01-31 18:44:00 -0500

    Add solution to 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let C = (F - 32)/9*5;
+  return Number(C.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let F = 32 + 9/5*C;
+  return Number(F.toFixed(1));
 };
 
 // Do not edit below this line
