cd1a947e05a7c721107874f9a3538f510dcf2758
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-02-25 15:51:55 -0500

    Add solution to exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let fibArray = [1, 1];
+  if (num == 0) {
+    return 0;
+  } else if (num == 1 || num == 2) {
+    return 1;
+  } else if (num > 2) {
+    for(i = 2; i < num; i++) {
+      fibArray.push(fibArray[i-2] + fibArray[i-1]);
+    }
+    return fibArray[num - 1];
+  } else {
+    return 'OOPS';
+  }
 };
 
 // Do not edit below this line
