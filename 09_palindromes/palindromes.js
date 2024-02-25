271cdbd64a0541751ec9d4da50064e78908f0652
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-02-25 12:33:02 -0500

    Add solution to exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let myRegex = /[a-z0-9]/g;
+  let newString = str.toLowerCase();
+  let newArray = newString.match(myRegex);
+  newString = newArray.join('');
+  let reversedArray = newArray.reverse();
+  let reversedString = reversedArray.join('');
+  if (newString === reversedString) {
+    return true;
+  } else {
+    return false;
+  }  
 };
 
 // Do not edit below this line
