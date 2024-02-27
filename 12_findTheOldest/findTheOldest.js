73ca50243e30c5648cf5af4ec37b5cdf606c7236
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-02-27 15:38:53 -0500

    Add solution to exercise 11_getTheTitles

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  array.map(function addAge(person) {
+      if(person.yearOfDeath == undefined) {
+        person.age = new Date().getFullYear() - person.yearOfBirth;
+    } else {
+        person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+});
+  const sortedArray = array.sort(function compareAge(a, b){
+      if ((a.age) > (b.age)) {
+      return -1;
+      }
+      if ((a.age) == (b.age)) {
+      return 0;
+      }
+      if ((a.age) < (b.age)) {
+      return 1;
+      }
+  });
+  return sortedArray[0];    
 };
 
 // Do not edit below this line
